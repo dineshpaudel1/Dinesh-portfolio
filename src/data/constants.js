c78f9ac0d38a26e9a272dfc0077457f942e97024
Dinesh Paudel <paudeldinesh961@gmail.com>
2023-12-11 22:37:48 +0545

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,211 @@
+export const Bio = {
+  name: "Dinesh Paudel",
+  roles: [
+    "Front End Developer",
+    "Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/dineshpaudel1",
+  resume:
+    "",
+  linkedin: "https://www.linkedin.com/in/dinesh-paudel-940b4b252/",
+  twitter: "https://twitter.com/?lang=en",
+  insta: "https://www.instagram.com/divashpaudel/",
+  facebook: "https://www.facebook.com/satiingraju.sattingraju/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "PHP",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/1067px-PHP-logo.svg.png",
+      },
+      
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Desegning ",
+    skills: [
+      {
+        name: "Photshop",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/2101px-Adobe_Photoshop_CC_icon.svg.png",
+      },
+      {
+        name: "Canva",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsJn9WTcNuoGcV98WFyIA-Mv_bdQOEMBpfczX2Z_66VQ&s",
+      },
+
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://cdn5.vectorstock.com/i/1000x1000/98/64/fitness-gym-logo-with-strong-athlete-and-barbell-vector-24189864.jpg",
+    role: "I have worked for My First Collage Project Which Name Called Gym Management System",
+    company: "Patan Multiple Campus Lalitpur",
+    date: "sepetember 2023 - November 2023",
+    desc: "",
+    skills: [
+      "Frontend Handler",
+      "Designer",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://jamanastudio.com/images/logo.png",
+    role: "I have worked for jamana studio website which is My First Outsource Project",
+    company: "Jamana studio Lalitpur",
+    date: "sepetember 2023 - November 2023",
+    desc: "Working on Jamana studio project i got chance to learn how to deal with Clients and Many More",
+    skills: [
+      "Frontend Handler",
+      "Designer",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://www.shutterstock.com/image-vector/vector-logo-school-260nw-427910128.jpg",
+    role: "As a Primary School Teacher",
+    company: "At Kailali",
+    date: "June 2023 - Present",
+    desc: "Working as a teacher i got chance to learn communication skill, leadership skill, Problem Solving Skill etc.",
+    skills: [
+      "Computer Science",
+      "Science",
+      "Social",
+      "Maths",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSF6GwoArknorOhIIPtmgVMjjv1y6qEwzTShMefZXWD7Q&s",
+    school: "GhodaGhodi Multiple Campus",
+    date: "jan 2017 - Sep 2019",
+    grade: "3.12 CGPA",
+    desc: "I have done my 11 & 12 at GhodaGhodi Multiple Campus",
+    degree: "",
+  },
+  {
+    id: 1,
+    img: "https://lh3.googleusercontent.com/p/AF1QipNjMpElSWUu08mbmgJ7hTdmDdARgcSomdAy6tTf=s1360-w1360-h1020",
+    school: "Patan Multiple Campus",
+    date: "Feb 2020 - Now",
+    grade: "coming soon%",
+    desc: "i am currently Studying Bachelor in Computer Application in Lalitpur",
+    degree: "Bachelor in Computer Application",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Gym Management System",
+    date: "May 2023 - December 2023",
+    description:
+      "Gym Management System is one of the online System Where user and Admin can interact with each other.",
+    image:
+    "https://themeforest.img.customer.envatousercontent.com/files/459791502/01_Preview.png?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=300&s=680c35e12dc2cee82a3c0444c32cbcd0",
+    tags: [
+      "HTML",
+      "CSS",
+      "JAVASCRIPT",
+      "PHP",
+      "MY SQL",
+    ],
+    category: "web app",
+    github: "https://github.com/dineshpaudel1/first-project",
+    webapp: "",
+  },
+  {
+    id: 0,
+    title: "Jamana Studio",
+    date: "july 2023 - Dec 2023",
+    description:
+      "I have worked in jamana company as a web developer",
+    image:
+      "https://lh3.googleusercontent.com/p/AF1QipN3YOGq0qqVGki8kf-e2CSkEP_9OjlWmTgTzJl-=s1360-w1360-h1020",
+    tags: ["Html", "CSS", "Javascript", "Laravel", "Mysql"],
+    category: "web app",
+    github: "https://github.com/dineshpaudel1/jamanastudio",
+    webapp: "https://jamanastudio.com/",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
